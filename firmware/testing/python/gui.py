4021641c32ecc48e0e55dc310d197357f02c6bba
Anshuman Fauzdar <fauzdaranshuman@gmail.com>
2022-01-09 14:08:24 +0530

    fix comparing values
    added comparison for less than value

--- firmware/testing/python/gui.py
+++ firmware/testing/python/gui.py
@@ -6,23 +6,23 @@
 from serial_read import serial_data_findbystring
 from guizero import App, Text, Picture
 
-THRESHOLD = 300.0
+THRESHOLD = 23.00
 
 # Action you would like to perform
 def value_update():
-    value = serial_data_findbystring("Pressure = ")
+    value = serial_data_findbystring("Temperature = ")
     text.value = value
     if (value and value > THRESHOLD):
         text.text_color = "#00ff00" # green
-        #picture.value = "pass-min.png"
-    else:
+        picture.value = "pass-min.png"
+    if (value and value < THRESHOLD):
         text.text_color = "#ff3333" # red
-        #picture.value = "fail-min.png"
+        picture.value = "fail-min.png"
 
 app = App("Predictive Maintenance", bg = "#121212") # #121212: black
 
 text = Text(app, text=value_update)
-#picture = Picture(app)
+picture = Picture(app)
 
 text.repeat(100, value_update)  # Schedule call to every "n"ms(1s = 1000ms)
 
